f56c4a5f7c2ab99305e37f71e8f0da3843a30679
Stephan Schultz <register@steppschuh.net>
2019-12-23 13:15:50 +0100

    Optionally set backend port from query parameter

--- Web-App/src/constants.js
+++ Web-App/src/constants.js
@@ -1,3 +1,1 @@
-const hostPort = "8080"; // TODO: check URL query parameter
-
-export const host = window.location.protocol + '//' + window.location.hostname + ':' + hostPort
+export const defaultBackendPort = 8080
