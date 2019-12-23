928a02aa7256534f91ba5cbfb4cb7f2f797b6bc4
Stephan Schultz <register@steppschuh.net>
2019-12-23 13:02:13 +0100

    Set backend host based on frontend hostname

--- Web-App/src/constants.js
+++ Web-App/src/constants.js
@@ -1,1 +1,3 @@
-export const host = "http://localhost:8080";
+const hostPort = "8080"; // TODO: check URL query parameter
+
+export const host = window.location.protocol + '//' + window.location.hostname + ':' + hostPort
